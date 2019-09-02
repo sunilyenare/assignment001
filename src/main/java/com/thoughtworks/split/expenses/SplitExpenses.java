759d1db621aae2e1aea7e013435d4184f20f4954
sunilsyenare <syenare97@gmail.com>
2019-09-02 10:57:19 +0530

    [Sunil] Update Class info

--- src/main/java/com/thoughtworks/split/expenses/SplitExpenses.java
+++ src/main/java/com/thoughtworks/split/expenses/SplitExpenses.java
@@ -2,15 +2,29 @@ package com.thoughtworks.split.expenses;
 
 import java.util.List;
 
+/**
+ * This class has following state and behaviour which help to split expenses equal
+ *
+ * #state#
+ * @param payerName
+ * @param amount
+ * @param grouplist
+ * @param payerPartOfExpenses
+ *
+ * #behaviour#
+ * checkSpendDetail
+ * checkIfPayerIsMember
+ * settelAccount
+ */
 public class SplitExpenses {
 
     private String payerName;
     private float amount;
     private List<String> groupList;
-    private  boolean payerPartOfExpense=false;
+    private boolean payerPartOfExpense = false;
 
-    public SplitExpenses(String payerName, boolean payerPartOfExpense,float amount, List<String> groupList) {
-        this.payerPartOfExpense=payerPartOfExpense;
+    public SplitExpenses(String payerName, boolean payerPartOfExpense, float amount, List<String> groupList) {
+        this.payerPartOfExpense = payerPartOfExpense;
         this.payerName = payerName;
         this.amount = amount;
         this.groupList = groupList;
@@ -21,8 +35,7 @@ public class SplitExpenses {
         List<String> totalMember = groupList;
         float amountToBePaid = 0.0f;
 
-        if (checkIfPayerIsMember()&&(!payerPartOfExpense)) totalMember.remove(payerName);
-
+        if (checkIfPayerIsMember() && (!payerPartOfExpense)) totalMember.remove(payerName);
 
 
         if (amount == 0) return amountToBePaid;
@@ -30,7 +43,7 @@ public class SplitExpenses {
         amountToBePaid = amount / totalMember.size();
 
 
-            settleAccount(amountToBePaid);
+        settleAccount(amountToBePaid);
         return amountToBePaid;
     }
 
@@ -45,7 +58,7 @@ public class SplitExpenses {
 
     public boolean settleAccount(float amountToBePaid) {
         for (String person : groupList) {
-            System.out.println(person + " should be paying "+ amountToBePaid + " to " + payerName);
+            System.out.println(person + " should be paying " + amountToBePaid + " to " + payerName);
         }
         return true;
     }
--- src/test/java/SplitExpensesTest.java
+++ src/test/java/SplitExpensesTest.java
@@ -7,69 +7,70 @@ import java.util.List;
 
 public class SplitExpensesTest {
     @Test
-    void givenZeroMember_WhenAdded_ThenShouldReturnZeroExpenses(){
-        List<String> groupList=new ArrayList<String>();
-        SplitExpenses manager=new SplitExpenses(" ", false,0.0f,groupList);
-        Assertions.assertEquals(0.0f,manager.checkSpendDetail());
+    void givenZeroMember_WhenAdded_ThenShouldReturnZeroExpenses() {
+        List<String> groupList = new ArrayList<String>();
+        SplitExpenses manager = new SplitExpenses(" ", false, 0.0f, groupList);
+        Assertions.assertEquals(0.0f, manager.checkSpendDetail());
     }
 
 
     @Test
-    void givenOneMember_WhenAddedAmount_ThenShouldReturnExpenses(){
-        List<String> groupList=new ArrayList<String>();
+    void givenOneMember_WhenAddedAmount_ThenShouldReturnExpenses() {
+        List<String> groupList = new ArrayList<String>();
         groupList.add("A");
-        SplitExpenses manager=new SplitExpenses("A",true,10.0f,groupList);
-        Assertions.assertEquals(10.0,manager.checkSpendDetail());
+        SplitExpenses manager = new SplitExpenses("A", true, 10.0f, groupList);
+        Assertions.assertEquals(10.0, manager.checkSpendDetail());
         groupList.clear();
     }
 
     @Test
-    void givenTwoMember_WhenAdded_ThenShouldReturnSplitExpenses(){
-        List<String> groupList=new ArrayList<String>();
+    void givenTwoMember_WhenAdded_ThenShouldReturnSplitExpenses() {
+        List<String> groupList = new ArrayList<String>();
         groupList.add("A");
         groupList.add("B");
-        SplitExpenses manager=new SplitExpenses("A",true,100.0f,groupList);
-        Assertions.assertEquals(50.0,manager.checkSpendDetail());
+        SplitExpenses manager = new SplitExpenses("A", true, 100.0f, groupList);
+        Assertions.assertEquals(50.0, manager.checkSpendDetail());
         groupList.clear();
     }
 
     @Test
-    void givenCheckMemberIsPayer_WhenAdded_ThenShouldReturnTrue(){
-        List<String> groupList=new ArrayList<String>();
+    void givenCheckMemberIsPayer_WhenAdded_ThenShouldReturnTrue() {
+        List<String> groupList = new ArrayList<String>();
         groupList.add("A");
         groupList.add("B");
         groupList.add("C");
         groupList.add("D");
-        SplitExpenses manager=new SplitExpenses("B",true,100.0f,groupList);
-        Assertions.assertEquals(true,manager.checkIfPayerIsMember());
+        SplitExpenses manager = new SplitExpenses("B", true, 100.0f, groupList);
+        Assertions.assertEquals(true, manager.checkIfPayerIsMember());
         groupList.clear();
 
     }
+
     @Test
-    void givenMemberIsPayer_WhenAdded_ThenShouldReturnEqualSplitExpenses(){
-        List<String> groupList=new ArrayList<String>();
+    void givenMemberIsPayer_WhenAdded_ThenShouldReturnEqualSplitExpenses() {
+        List<String> groupList = new ArrayList<String>();
         groupList.add("A");
         groupList.add("B");
         groupList.add("C");
         groupList.add("D");
         groupList.add("E");
         groupList.add("F");
-        SplitExpenses manager=new SplitExpenses("A",true,360.0f,groupList);
-        Assertions.assertEquals(60,manager.checkSpendDetail());
+        SplitExpenses manager = new SplitExpenses("A", true, 360.0f, groupList);
+        Assertions.assertEquals(60, manager.checkSpendDetail());
         groupList.clear();
 
     }
 
     @Test
-    void givenMembersIsPayerButNotAPartOfExpense_WhenAdded_ThenShouldReturnNotEqualSplitExpenses(){
-        List<String> groupList=new ArrayList<String>();
+    void givenMembersIsPayerButNotAPartOfExpense_WhenAdded_ThenShouldReturnNotEqualSplitExpenses() {
+        List<String> groupList = new ArrayList<String>();
         groupList.add("A");
         groupList.add("B");
         groupList.add("C");
         groupList.add("D");
         groupList.add("E");
-        SplitExpenses manager=new SplitExpenses("A",false,100.0f,groupList);
-        Assertions.assertEquals(25,manager.checkSpendDetail());
+        SplitExpenses manager = new SplitExpenses("A", false, 100.0f, groupList);
+        Assertions.assertEquals(25, manager.checkSpendDetail());
         groupList.clear();
 
     }
